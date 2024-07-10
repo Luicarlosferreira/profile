6162db767f2ea5f0f8f9b3ff940dbeb58cb08748
Luis Carlos ferreira carneiro <luicarlosferreira@outlook.com>
2024-07-10 17:28:15 -0400

    teste: prod

--- app/components/projectsCard.tsx
+++ app/components/projectsCard.tsx
@@ -6,7 +6,7 @@ type ITittle = {
 
 const ProjectCards = ({ title }: ITittle) => {
   return (
-    <div className="p-5 w-52  animate-[pulse_1.5s_linear] h-28 dark:bg-neutral-500 border border-neutral-500  rounded-sm flex align-middle items-center justify-center hover:border-blue-500 lg:w-96 lg:h-44 sm:w-full ">
+    <div className="p-5   animate-[pulse_1.5s_linear] h-28 dark:bg-neutral-500 border border-neutral-500  rounded-sm flex align-middle items-center justify-center hover:border-blue-500 lg:w-96 lg:h-44 sm:w-full ">
       <span className="text-center transition-all hover:text-blue-500 sm:text-xl ">{title}</span>
     </div>
   );
